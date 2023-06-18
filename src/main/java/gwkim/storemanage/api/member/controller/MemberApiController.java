baa0d072174472f4f70f35dfcf75d5b493818f34
gunwookim <gunwo151@gmail.com>
2023-06-18 18:35:15 +0900

    깃 테스트

--- src/main/java/gwkim/storemanage/api/member/controller/MemberApiController.java
+++ src/main/java/gwkim/storemanage/api/member/controller/MemberApiController.java
@@ -39,6 +39,12 @@ public class MemberApiController {
      * - 엔티티가 변경되면 API 스펙이 변한다.
      * 결론
      * - API 요청 스펙에 맞추어 별도의 DTO를 파라미터로 받는다.
+     *
+     *
+     *
+     *
+     *
+     *
      */
     @PostMapping("/api/v1/members")
     public MemberApiResponse<String> saveMemberV2(@RequestBody @Valid MemberSignUpDto memberDto) {
